c7137135a557bb3ab51061613cebfd8263b177be
fredrvaa <fredrvaa@stud.ntnu.no>
2019-10-09 15:41:48 +0200

    predict.py now saves masks on top of images

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-import os,time,cv2, sys, math
+import os,cv2,sys,math
 import tensorflow as tf
 import argparse
 import numpy as np
@@ -7,13 +7,17 @@ from utils import utils, helpers
 from builders import model_builder
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image', type=str, default=None, required=True, help='The image you want to predict on. ')
+parser.add_argument('--image', type=str, default=None, required=False, help='The image you want to predict on. ')
+parser.add_argument('--folder', type=str, default=None, required=False, help='The folder you want to predict on')
 parser.add_argument('--checkpoint_path', type=str, default=None, required=True, help='The path to the latest checkpoint weights for your model.')
 parser.add_argument('--crop_height', type=int, default=512, help='Height of cropped input image to network')
 parser.add_argument('--crop_width', type=int, default=512, help='Width of cropped input image to network')
-parser.add_argument('--model', type=str, default=None, required=True, help='The model you are using')
+parser.add_argument('--model', type=str, default='FC-DenseNet56', required=False, help='The model you are using')
 parser.add_argument('--dataset', type=str, default="CamVid", required=False, help='The dataset you are using')
 args = parser.parse_args()
+
+
+assert args.image or args.folder, "Argument --image or --folder is required for prediciting"
 
 class_names_list, label_values = helpers.get_label_info(os.path.join(args.dataset, "class_dict.csv"))
 
@@ -51,20 +55,36 @@ saver.restore(sess, args.checkpoint_path)
 print("Testing image " + args.image)
 
 loaded_image = utils.load_image(args.image)
-resized_image =cv2.resize(loaded_image, (args.crop_width, args.crop_height))
+orig_image = cv2.cvtColor(loaded_image, cv2.COLOR_RGB2BGR)  
+resized_image = cv2.resize(loaded_image, (args.crop_width, args.crop_height))
 input_image = np.expand_dims(np.float32(resized_image[:args.crop_height, :args.crop_width]),axis=0)/255.0
 
-st = time.time()
 output_image = sess.run(network,feed_dict={net_input:input_image})
-
-run_time = time.time()-st
 
 output_image = np.array(output_image[0,:,:,:])
 output_image = helpers.reverse_one_hot(output_image)
 
-out_vis_image = helpers.colour_code_segmentation(output_image, label_values)
+mask = helpers.colour_code_segmentation(output_image, label_values)
+mask = cv2.cvtColor(np.uint8(mask), cv2.COLOR_RGB2BGR)
+
+resized_mask = cv2.resize(mask, (loaded_image.shape[1], loaded_image.shape[0]))
+
+pred_fg = np.copy(resized_mask)
+pred_fg[np.where((pred_fg==[255,255,255]).all(axis=2))] = [255,0,0]
+pred_fg[np.where((pred_fg!=[255,0,0]).all(axis=2))] = [0,0,0]
+
+cv2.imshow('pred', pred_fg)
+cv2.waitKey(0)
+
+resized_mask = cv2.cvtColor(resized_mask, cv2.COLOR_BGR2GRAY)
+
+mask_inv = cv2.bitwise_not(resized_mask)    
+bg = cv2.bitwise_or(orig_image, orig_image, mask=mask_inv)
+
+final = cv2.bitwise_or(pred_fg, bg)
+
 file_name = utils.filepath_to_name(args.image)
-cv2.imwrite("%s_pred.png"%(file_name),cv2.cvtColor(np.uint8(out_vis_image), cv2.COLOR_RGB2BGR))
+cv2.imwrite("pred_images/{}_pred.png".format(file_name), final)
 
 print("")
 print("Finished!")
